21b6e165f9e1165668dd8cb087ae2aa968614c38
imranpanjwani <imranpanjwani00@gmail.com>
2020-03-22 14:26:40 +0530

    add schema

---
+++ src/schemas/Project.ts
@@ -0,0 +1,15 @@
+import { ObjectType, Field, Int } from 'type-graphql';
+import Task from './Task';
+
+@ObjectType()
+export default class Project {
+
+    @Field(type => Int)
+    id: number;
+
+    @Field()
+    name: string;
+
+    @Field(type => [Task])
+    tasks: Task[];
+}
---
+++ src/schemas/Task.ts
@@ -0,0 +1,18 @@
+import { ObjectType, Field, Int } from "type-graphql";
+import Project from "./Project";
+
+@ObjectType()
+export default class Task {
+
+    @Field(type => Int)
+    id: number;
+
+    @Field()
+    title: string;
+
+    @Field(type => Project)
+    project: Project;
+
+    @Field()
+    completed: boolean;
+}
